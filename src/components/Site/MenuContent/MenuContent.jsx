d59c3b3b03a0fc0f874dfc3a4d2656a4dc1e81f8
hoatv02 <105353827+hoatv02@users.noreply.github.com>
2022-07-11 15:14:24 +0700

    updateHOme

---
+++ src/components/Site/MenuContent/MenuContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MenuContent() {
+  return (
+    <div className='row'>
+        <div className='col-lg-3'></div>
+        <div className='col-lg-9'></div>
+    </div>
+  )
+}
+
+export default MenuContent
