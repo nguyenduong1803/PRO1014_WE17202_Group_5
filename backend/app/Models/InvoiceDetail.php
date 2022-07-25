76e15f7925b67ba895d421fad7fea029fbadda5c
DuongLT <duonglt.work@gmail.com>
2022-07-25 22:33:40 +0700

    api detailInvoice

--- backend/app/Models/InvoiceDetail.php
+++ backend/app/Models/InvoiceDetail.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class InvoiceDetail extends Model
 {
     use HasFactory;
+    public function getDetailInvoice() {
+                    return DB::select("SELECT
+                p.id as id_product,
+                cart.amount,
+                p.name
+            FROM
+                cart
+            INNER JOIN products AS p
+            ON
+                p.id = cart.id_product
+            WHERE
+                cart.id_user = 5 AND cart.is_delete = 1 AND p.is_delete = 1");
+    }
 }
