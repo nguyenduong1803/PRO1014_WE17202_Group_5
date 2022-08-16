28d35d49686ec38664953963bc4c783de19cf3f9
DuongLT <letrungduongxx3@gmail.com>
2022-08-17 00:12:27 +0700

    update logic

--- backend/config/database.php
+++ backend/config/database.php
@@ -45,6 +45,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'modes' => [
+                'STRICT_ALL_TABLES',
+            ],
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
